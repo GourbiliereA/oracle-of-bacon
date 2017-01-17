b830d4e1e19bb7bfec5ef4b4f13edd86f5524789
Pierre Cailleaud <pierre.cailleaud@gmail.com>
2017-01-17 13:30:29 +0100

    Implémentation de l'Oracle de Bacon avec Neo4J + correction Readme

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
@@ -1,11 +1,20 @@
 package com.serli.oracle.of.bacon.repository;
 
 
+import org.neo4j.driver.internal.InternalPath;
+import org.neo4j.driver.internal.value.PathValue;
 import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Relationship;
 
+import static org.neo4j.driver.v1.Values.parameters;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,14 +22,50 @@ public class Neo4JRepository {
     private final Driver driver;
 
     public Neo4JRepository() {
-        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "neo4j"));
     }
 
-    public List<?> getConnectionsToKevinBacon(String actorName) {
+    public List<GraphData> getConnectionsToKevinBacon(String actorName) {
         Session session = driver.session();
-
-        // TODO implement Oracle of Bacon
-        return null;
+        
+        // Request database
+        StatementResult result = session.run("MATCH path=shortestpath((bacon:Actor)-[*0..]-(xx:Actor)) "
+        		+ "WHERE bacon.name = 'Bacon, Kevin (I)' AND xx.name =~ {actorname} RETURN path;", 
+        		parameters("actorname", actorName));
+        
+        List<GraphData> listData = new ArrayList<GraphData>();
+        
+        while (result.hasNext())
+        {
+            Record record = result.next();
+            PathValue path = (PathValue) record.get("path");
+            InternalPath ipath = (InternalPath) path.asPath();
+            
+            for (Node node : ipath.nodes()) {
+            	GraphNode gNode = null;
+            	if(node.hasLabel("Movie")) {
+            		gNode = new GraphNode(node.id(), node.get("title").toString(), "Movie");
+            	}
+            	else if(node.hasLabel("Actor")) {
+            		gNode = new GraphNode(node.id(), node.get("name").toString(), "Actor");
+            	}
+            	listData.add(new GraphData(gNode));
+			}
+            for (Relationship relationship : ipath.relationships()) {
+				GraphEdge edge = new GraphEdge(relationship.id(), relationship.startNodeId(), relationship.endNodeId(), relationship.type());
+				listData.add(new GraphData(edge));
+            }
+        }
+        session.close();
+        return listData;
+    }
+    
+    private static class GraphData {
+    	public final GraphItem data;
+    	
+    	 private GraphData(GraphItem data) {
+             this.data = data;
+         }
     }
 
     private static abstract class GraphItem {
